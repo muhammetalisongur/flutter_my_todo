644c3fd5b80078aa865f28103ec2accbc7dd632e
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2022-12-16 14:51:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login.dart';
+import 'master_class.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      home: const MasterClass(),
     );
   }
 }
